71ea1c5655417fe5e522714757db1a9351a7ac4c
Aaron Hanrahan <hanrahanaaron@gmail.com>
2020-01-20 21:12:26 -0600

    Finished basic rock paper scissors.

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -28,10 +28,10 @@ function playRound(computerSelection, playerSelection) {
     if (computerSelection == playerSelection) {
         return "It's a tie!";
     } else if (doesCompWin(computerSelection, playerSelection)) {
-        playerScore++;
+        computerScore++;
         return "You lose! " + computerSelection + " beats " + playerSelection + ".";
     } else {
-        computerScore++;
+        playerScore++;
         return "You win! " + playerSelection + " beats " + computerSelection + ".";
     }
 }
@@ -46,18 +46,49 @@ function doesCompWin(cInp, pInp) {
     return false;
 }
 
-// Runs 5 games of rps.
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Choose rock, paper, or scissors.");
-        let computerSelection = computerPlay();
-        console.log(playRound(computerSelection, playerSelection));
-    }
-    if (playerScore > computerScore) {
-        console.log("Player wins with a score of " + playerScore + " to " + computerScore);
-    } else if (computerScore > playerScore) {
-        console.log("Computer wins with a score of " + computerScore + " to " + playerScore);
-    } else {
-        console.log("It is a tie with a score of " + playerScore + " to " + computerScore);
-    }
-}
+const results = document.querySelector('#results');
+const buttons = document.querySelectorAll('button');
+let temp_results;
+let game_score;
+let temp_results_text;
+let game_score_text;
+
+// Iterate through each button.
+buttons.forEach((button) => {
+
+    // Listen for each button being clicked.
+    button.addEventListener('click', (e) => {
+
+        if (document.getElementById("temporary-results") == null
+            && document.getElementById("game-scores") == null) {
+                temp_results = document.createElement("div");
+                game_score = document.createElement("div");
+                temp_results.id = 'temporary-results';
+                game_score.id = 'game-scores';
+                results.appendChild(temp_results);
+                results.appendChild(game_score);
+                temp_results_text = document.createElement('p');
+                game_score_text = document.createElement('p');
+                temp_results_text.style = 'text-align: center';
+                game_score_text.style = 'text-align: center';
+                temp_results.appendChild(temp_results_text);
+                game_score.appendChild(game_score_text);
+        }
+        let player_choice = button.id;
+        temp_results_text.textContent = playRound(computerPlay(), player_choice);
+        game_score_text.textContent = 'Player: ' + playerScore + ' Computer: ' + computerScore;
+
+        if (playerScore == 5 || computerScore == 5) {
+            if (playerScore == 5) {
+                alert('You win! Click ok to play again.');
+            } else {
+                alert('You lose! Click ok to try again.');
+            }
+            temp_results_text.textContent = '';
+            game_score_text.textContent = '';
+            playerScore = 0;
+            computerScore = 0;
+        }
+    });
+
+});
